69d9ba0bd8baeb8e311746c89a869d47757d1381
samin <samincgs@gmail.com>
2024-05-02 17:57:37 -0400

    users can now use openAI to generate answers, still need to display on UI

--- utils/actions.ts
+++ utils/actions.ts
@@ -7,5 +7,15 @@ const openai = new OpenAI({
 });
 
 export const generateChatResponse = async (chatMessage: any) => {
-  console.log(chatMessage);
+  const response = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: 'you are a helpful assistant' },
+      { role: 'user', content: chatMessage },
+    ],
+    model: 'gpt-3.5-turbo',
+    temperature: 0,
+  });
+
+  console.log(response.choices[0].message);
+  console.log(response);
 };
