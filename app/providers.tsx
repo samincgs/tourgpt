d0d2da186c77128a4231a993ea472e4ea7a76222
samin <samincgs@gmail.com>
2024-05-02 14:33:21 -0400

    added react query boilerplate

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,28 @@
 'use client';
 
 import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster position='top-center' />
+      <ReactQueryDevtools initialIsOpen={false} />
       {children}
-    </>
+    </QueryClientProvider>
   );
 };
 export default Providers;
