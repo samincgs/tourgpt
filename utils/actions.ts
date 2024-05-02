e1e4f1c8d35340d8c58371d1b80fdc026ad6cd08
samin <samincgs@gmail.com>
2024-05-02 16:31:21 -0400

    started utilizing the openAI api

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,11 @@
 'use server';
 
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 export const generateChatResponse = async (chatMessage: any) => {
   console.log(chatMessage);
 };
