fb4b6d75ea6e44deb191bb11adcf25c51a72c46b
samin <samincgs@gmail.com>
2024-05-02 20:47:49 -0400

    created input to get new tours from users

--- components/NewTour.tsx
+++ components/NewTour.tsx
@@ -1,4 +1,45 @@
+'use client';
+import { FormEvent, FormEventHandler } from 'react';
+import TourInfo from './TourInfo';
+
 const NewTour = () => {
-  return <div className='pt-12'>NewTour</div>;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log(destination);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='pt-12 max-w-2xl'>
+        <h2 className='capitalize mb-8 font-bold text-accent text-3xl tracking-wide'>
+          plan your dream vacation
+        </h2>
+        <div className='join w-full'>
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='city'
+            required
+            name='city'
+          />
+          <input
+            type='text'
+            className='input input-bordered join-item w-full'
+            placeholder='country'
+            required
+            name='country'
+          />
+          <button type='submit' className='btn btn-primary join-item uppercase'>
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className='mt-16'>
+        <TourInfo />
+      </div>
+    </>
+  );
 };
 export default NewTour;
